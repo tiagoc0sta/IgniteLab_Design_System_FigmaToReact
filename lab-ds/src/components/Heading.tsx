036256e935c9f66924bdf719923d4a482038e1e3
Tiago Costa <tiagodpv@gmail.com>
2022-10-12 16:20:48 -0400

    edit stoytelling

--- lab-ds/src/components/Heading.tsx
+++ lab-ds/src/components/Heading.tsx
@@ -2,23 +2,23 @@ import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx';
 import { ReactNode } from 'react';
 
-export interface TextProps {
+export interface HeadingProps {
   size?:'sm' | 'md' | 'lg';
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Text({size ='md', children, asChild}: TextProps) {
-  const Comp = asChild ? Slot: 'span';
+export function Heading({size ='md', children, asChild}: HeadingProps) {
+  const Comp = asChild ? Slot: 'h2';
 
   return(
     <Comp 
       className={clsx(
-        'text-gray-100 font-sans',
+        'text-gray-100 font-bold font-sans',
         {
-          'text-xs': size === 'sm',
-          'text-sm': size === 'md',
-          'text-md': size === 'lg',
+          'text-lg': size === 'sm',
+          'text-xl': size === 'md',
+          'text-2xl': size === 'lg',
         } 
       )}
     >
