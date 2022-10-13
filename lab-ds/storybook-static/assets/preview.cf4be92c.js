d882c2c3a9edf72b09e00c94486d130b9fb9d167
Tiago Costa <tiagodpv@gmail.com>
2022-10-13 11:18:20 -0400

    last change

---
+++ lab-ds/storybook-static/assets/preview.cf4be92c.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.bb551f93.js";import"./iframe.5e3092ab.js";import"./es.map.constructor.5ae4fa2a.js";import"./es.number.to-fixed.e47f673e.js";import"./index.10a028a9.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.cf4be92c.js.map
